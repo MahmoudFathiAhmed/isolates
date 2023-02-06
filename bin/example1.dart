09f88fe87c3cf728357e3011fae47669418f5af8
MahmoudFathiAhmed <zanoon.dx@gmail.com>
2023-02-06 17:11:08 +0200

    ####

---
+++ bin/example1.dart
@@ -0,0 +1,34 @@
+import 'dart:isolate';
+
+void main(List<String> args) async {
+  await for (final msg in getMessages().take(10)) {
+    print(msg);
+  }
+}
+
+Stream<String> getMessages(){
+  final rp = ReceivePort();
+  return Isolate.spawn(_getMessages, rp.sendPort)
+      .asStream()
+      .asyncExpand((_) => rp)
+      .takeWhile((element) => element is String)
+      .cast();
+}
+/*
+Stream<String> getMessages() async* {
+  final rp = ReceivePort();
+  await Isolate.spawn(_getMessages, rp.sendPort);
+  await for (final msg in rp.takeWhile((element) => element is String).cast<String>()) {
+    yield msg;
+  }
+}
+ */
+
+void _getMessages(SendPort sp) async {
+  await for (final now in Stream.periodic(
+    const Duration(milliseconds: 200),
+    (_) => DateTime.now().toIso8601String(),
+  )) {
+    sp.send(now);
+  }
+}
